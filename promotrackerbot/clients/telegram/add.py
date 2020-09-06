28d6a11bb1e108c6fdc5860dd26f1fb146ab1b19
Gabriel Pires <gabrielpires@dcc.ufmg.br>
2020-09-05 22:43:13 -0300

    /add command function

---
+++ promotrackerbot/clients/telegram/add.py
@@ -0,0 +1,10 @@
+def add(update, context):
+    if len(context.args) == 0:
+        empty_msg_error = ("Can't add \'nothing\', "
+        +"please type /add <product url>.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text=empty_msg_error)
+    else:
+        product_name = context.args[0]
+        answer = f"Product {product_name} added!"
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
