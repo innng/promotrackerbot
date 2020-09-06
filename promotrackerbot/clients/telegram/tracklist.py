536c7ee693afbb11a67c152bcf90a4e117b2737b
Gabriel Pires <gabrielpires@dcc.ufmg.br>
2020-09-05 22:43:48 -0300

    /tracklist command function

---
+++ promotrackerbot/clients/telegram/tracklist.py
@@ -0,0 +1,7 @@
+def tracklist(update, context):
+    product_list = ["Placeholder1", "Placeholder2"]
+    answer = ("These are the products you are tracking: "
+    +", ".join(product_list)
+    +".")
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
