0d6cdade72c3585327d27191a486e7783094f2b4
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-09-10 18:05:31 -0300

    Modify greeting in the /start command

--- promotrackerbot/clients/telegram/start.py
+++ promotrackerbot/clients/telegram/start.py
@@ -10,28 +10,34 @@ def start(update, context):
     if today == now.day and 5 <= hour < 12:
         context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="Good morning! Welcome to promobot, the easy solution to track steam games prices",
+            text="Good morning! Welcome to promobot, the easy solution to track steam game prices",
         )
     elif today == now.day and 12 <= hour < 17:
         context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="Good afternoon! Welcome to promobot, the easy solution to track steam games "
+            text="Good afternoon! Welcome to promobot, the easy solution to track steam game "
             + "prices",
         )
     elif today == now.day and 17 <= hour < 24:
         context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="Good night! Welcome to promobot, the easy solution to track steam games prices",
+            text="Good night! Welcome to promobot, the easy solution to track steam game prices",
         )
     elif today == now.day and 0 <= hour < 5:
         context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="Good night! Welcome to promobot, the easy solution to track steam games prices",
+            text="Good night! Welcome to promobot, the easy solution to track steam game prices",
         )
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Please enter the desired option:\n"
-        + "/help to see all the options\n"
-        + "/add to add a new product\n"
-        + "/tracklist to see all the registered products\n",
+        text="How it works:\n"
+        + "You search for your favorite game in the Steam "
+        + "game database with the /add command.\n"
+        + "Set the amount you want to pay for the game and " 
+        + "the bot will send a message when the game reaches "
+        + "the selected or lower price.\n"
+        + "You can track how many games do you want.\n"
+        + "To view and manage all the games on your wish list, "
+        + "enter the command /tracklist.\n"
+        + "To see all the commands in detail, type the command /help",        
     )
