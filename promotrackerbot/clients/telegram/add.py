80ba497da5aab09a71a459e27cdc7e29da77fe0c
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-09-11 12:28:17 -0300

    Change /add answer format

--- promotrackerbot/clients/telegram/add.py
+++ promotrackerbot/clients/telegram/add.py
@@ -3,7 +3,12 @@ def add(update, context):
         empty_msg_error = "Can't add 'nothing', " + "please type /add <product url>."
         context.bot.send_message(chat_id=update.effective_chat.id, text=empty_msg_error)
     else:
+        size_name = len(context.args)
         product_name = context.args[0]
+        for position in range(1, size_name):
+            if position != size_name:
+                product_name = product_name + " "
+            product_name = product_name + context.args[position]
         answer = f"Product {product_name} added!"
 
         context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
