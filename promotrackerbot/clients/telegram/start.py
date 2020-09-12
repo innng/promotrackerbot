7789888ed92055ab9d39f738927393ee88a25214
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-09-12 12:31:33 -0300

    Unit tests for the /start command

--- promotrackerbot/clients/telegram/start.py
+++ promotrackerbot/clients/telegram/start.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 now = datetime.datetime.now()
 
@@ -7,30 +8,7 @@ hour = now.hour
 
 
 def start(update, context):
-    if today == now.day and 5 <= hour < 12:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Good morning! Welcome to promobot, the easy solution to track steam game prices",
-        )
-    elif today == now.day and 12 <= hour < 17:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Good afternoon! Welcome to promobot, the easy solution to track steam game "
-            + "prices",
-        )
-    elif today == now.day and 17 <= hour < 24:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Good night! Welcome to promobot, the easy solution to track steam game prices",
-        )
-    elif today == now.day and 0 <= hour < 5:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Good night! Welcome to promobot, the easy solution to track steam game prices",
-        )
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="How it works:\n\n"
+    text_instruction=("How it works:\n\n"
         + "You search for your favorite game in the Steam "
         + "game database with the /add command.\n\n"
         + "Set the amount you want to pay for the game and "
@@ -39,5 +17,29 @@ def start(update, context):
         + "You can track how many games do you want.\n\n"
         + "To view and manage all the games on your wish list, "
         + "enter the command /tracklist.\n\n"
-        + "To see all the commands in detail, type the command /help",
-    )
+        + "To see all the commands in detail, type the command /help")
+
+    if today == now.day and 5 <= hour < 12:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=("Good morning! Welcome to promobot, the easy solution to track steam game "
+                + "prices\n\n" + text_instruction),
+        )
+    elif today == now.day and 12 <= hour < 17:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=("Good afternoon! Welcome to promobot, the easy solution to track steam game "
+                + "prices\n\n" + text_instruction),
+        )
+    elif today == now.day and 17 <= hour < 24:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=("Good night! Welcome to promobot, the easy solution to track steam game "
+                + "prices\n\n" + text_instruction),
+        )
+    elif today == now.day and 0 <= hour < 5:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=("Good night! Welcome to promobot, the easy solution to track steam game "
+                + "prices\n\n" + text_instruction),
+        )
---
+++ tests/unit/telegram/test_start.py
@@ -0,0 +1,126 @@
+from promotrackerbot.clients.telegram.start import start
+from unittest.mock import Mock
+
+import datetime
+import time
+
+now = datetime.datetime.now()
+
+today = now.day
+hour = now.hour
+
+text_instruction=("How it works:\n\n"
+    + "You search for your favorite game in the Steam "
+    + "game database with the /add command.\n\n"
+    + "Set the amount you want to pay for the game and "
+    + "the bot will send a message when the game reaches "
+    + "the selected or lower price.\n\n"
+    + "You can track how many games do you want.\n\n"
+    + "To view and manage all the games on your wish list, "
+    + "enter the command /tracklist.\n\n"
+    + "To see all the commands in detail, type the command /help")
+
+if today == now.day and 5 <= hour < 12:
+    def test_start_morning():
+        # Arrange
+        chat_id = 999
+        response = ("Good morning! Welcome to promobot, the easy solution to track steam game "
+            + "prices\n\n" + text_instruction)
+
+        update = Mock()
+        update.effective_chat.id = chat_id
+
+        context = Mock()
+
+        result = {}
+
+        def get_bot_answer(**kwargs):
+            result.update(kwargs)
+
+        context.bot.send_message = get_bot_answer
+
+        # Act
+        start(update, context)
+
+        # Assert
+        assert result["chat_id"] == chat_id
+        assert result["text"] == response
+
+
+elif today == now.day and 12 <= hour < 17:
+    def test_start_afternoon():
+        # Arrange
+        chat_id = 999
+        response = ("Good afternoon! Welcome to promobot, the easy solution to track steam game "
+            + "prices\n\n" + text_instruction)
+
+        update = Mock()
+        update.effective_chat.id = chat_id
+
+        context = Mock()
+
+        result = {}
+
+        def get_bot_answer(**kwargs):
+            result.update(kwargs)
+
+        context.bot.send_message = get_bot_answer
+
+        # Act
+        start(update, context)
+
+        # Assert
+        assert result["chat_id"] == chat_id
+        assert result["text"] == response
+
+elif today == now.day and 17 <= hour < 24:
+    def test_start_night():
+        # Arrange
+        chat_id = 999
+        response = ("Good night! Welcome to promobot, the easy solution to track steam game "
+            + "prices\n\n" + text_instruction)
+
+        update = Mock()
+        update.effective_chat.id = chat_id
+
+        context = Mock()
+
+        result = {}
+
+        def get_bot_answer(**kwargs):
+            result.update(kwargs)
+
+        context.bot.send_message = get_bot_answer
+
+        # Act
+        start(update, context)
+
+        # Assert
+        assert result["chat_id"] == chat_id
+        assert result["text"] == response
+
+elif today == now.day and 0 <= hour < 5:
+    def test_start_dawn():
+        # Arrange
+        chat_id = 999
+        response = ("Good night! Welcome to promobot, the easy solution to track steam game "
+            + "prices\n\n" + text_instruction)
+
+        update = Mock()
+        update.effective_chat.id = chat_id
+
+        context = Mock()
+
+        result = {}
+
+        def get_bot_answer(**kwargs):
+            result.update(kwargs)
+
+        context.bot.send_message = get_bot_answer
+
+        # Act
+        start(update, context)
+
+        # Assert
+        assert result["chat_id"] == chat_id
+        assert result["text"] == response
