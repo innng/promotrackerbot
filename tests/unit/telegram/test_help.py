be57abc2ba3bbb8b2f8212c286438cebb0f592c7
Gabriel Pires <gabrielpires@dcc.ufmg.br>
2020-09-11 21:43:50 -0300

    Unit test for the /help command

---
+++ tests/unit/telegram/test_help.py
@@ -0,0 +1,33 @@
+from promotrackerbot.clients.telegram.help import help
+from unittest.mock import Mock
+
+
+def test_help():
+    # Arrange
+    chat_id = 999
+    response = (
+        "I can help you find out when a game is on sale!\n\n"
+        + "You can control me by sending these commands:\n"
+        + "/start - start a chat with me\n"
+        + "/help - show a list of commands\n\n"
+        + "New commands will be available soon."
+    )
+
+    update = Mock()
+    update.effective_chat.id = chat_id
+
+    context = Mock()
+
+    result = {}
+
+    def get_bot_answer(**kwargs):
+        result.update(kwargs)
+
+    context.bot.send_message = get_bot_answer
+
+    # Act
+    help(update, context)
+
+    # Assert
+    assert result["chat_id"] == chat_id
+    assert result["text"] == response
