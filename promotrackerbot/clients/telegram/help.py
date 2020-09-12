1bffe1de888387d284aac9cd5f549a72cff6ebae
Gabriel Pires <gabrielpires@dcc.ufmg.br>
2020-09-11 21:42:58 -0300

    Fixed a typo

--- promotrackerbot/clients/telegram/help.py
+++ promotrackerbot/clients/telegram/help.py
@@ -2,7 +2,7 @@ def help(update, context):
     cmd_list = (
         "I can help you find out when a game is on sale!\n\n"
         + "You can control me by sending these commands:\n"
-        + "/start - start achat with me\n"
+        + "/start - start a chat with me\n"
         + "/help - show a list of commands\n\n"
         + "New commands will be available soon."
     )
