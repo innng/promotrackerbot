7caabd0a325b9bc374b527882dc361a31a8fcb4a
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-10-19 19:37:31 -0300

    Modify help message

--- promotrackerbot/clients/telegram/help.py
+++ promotrackerbot/clients/telegram/help.py
@@ -3,9 +3,11 @@ def help(update, context):
         "I can help you find out when a game is on sale!\n\n"
         + "You can control me by sending these commands:\n"
         + "/start - start a chat with me\n"
-        + "/help - shows the list of commands\n\n"
-        + "/add <steam game url> - add a game to your track list"
-        + "/tracklist - shows all the games you're tracking"
+        + "/help - shows the list of commands\n"
+        + "/add <steam game url> - add a game to your track list\n"
+        + "/tracklist - shows all the games you're tracking\n"
+        + "/remove - delete a specific game from tracklist\n"
+        + "/removeall - delete the tracklist and all the games within it\n\n"
         + "New commands will be available soon."
     )
 
