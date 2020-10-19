51acab6251e9653fa1f9650355119509f6d7c2e0
Ingrid Rosselis <ingridrosselis@gmail.com>
2020-10-19 13:03:53 -0300

    Implement unit tests for tracklist

--- promotrackerbot/clients/telegram/tracklist.py
+++ promotrackerbot/clients/telegram/tracklist.py
@@ -3,16 +3,33 @@ from promotrackerbot.infra.redis import RedisClient
 # import telegram
 
 
-# def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
-#     menu = [buttons[i : i + n_cols] for i in range(0, len(buttons), n_cols)]
-#     if header_buttons:
-#         menu.insert(0, [header_buttons])
-#     if footer_buttons:
-#         menu.append([footer_buttons])
-#     return menu
-
-
 def tracklist(update, context):
+    redis = RedisClient()
+    redis.connect()
+
+    chat_id = update.effective_chat.id
+    if not redis.exists(chat_id) or not redis.get(chat_id):
+        answer = "No games are being tracked."
+        context.bot.send_message(chat_id=chat_id, text=answer)
+        return
+
+    game_list = redis.get(str(chat_id))
+    product_list = ""
+
+    for appid in game_list:
+        product_list += game_list[appid]["name"] + " - " + game_list[appid]["price_formated"]
+        if game_list[appid]["discount"] > 0:
+            product_list += " - <b>Discount: " + str(game_list[appid]["discount"]) + "% !!</b> "
+        else:
+            product_list += " - No discount active. "
+        store_link = "https://store.steampowered.com/app/" + appid
+        product_list += "<a href='" + store_link + "'> Link</a>\n"
+
+    answer = "These are the products you are tracking: " + "\n\n" + product_list
+
+    context.bot.send_message(
+        chat_id=chat_id, text=answer, parse_mode="HTML", disable_web_page_preview=True,
+    )
 
     # button_list = [
     #     telegram.InlineKeyboardButton("Yes", callback_data="dlc-yes"),
@@ -25,34 +42,12 @@ def tracklist(update, context):
     #     reply_markup=reply_markup,
     # )
 
-    redis = RedisClient()
-    redis.connect()
+    # def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
 
-    chat_id = update.effective_chat.id
-    if not redis.exists(chat_id) or redis.get(chat_id) == {}:
-        answer = "No games are being tracked."
-        context.bot.send_message(chat_id=chat_id, text=answer)
-        return
 
-    game_list = redis.get(str(chat_id))
-    # print(game_list, flush=True)
-    product_list = ""
-
-    for appid in game_list:
-        product_list += game_list[appid]["name"] + " - " + game_list[appid]["price_formated"]
-        if game_list[appid]["discount"] > 0:
-            product_list += " - <b>Discount: " + str(game_list[appid]["discount"]) + "% !!</b> "
-        else:
-            product_list += " - No discount active. "
-        store_link = "https://store.steampowered.com/app/" + appid
-        product_list += "<a href='" + store_link + "'> Link</a>\n"
-    # print(product_list, flush=True)
-
-    answer = "These are the products you are tracking: " + "\n\n" + product_list
-
-    context.bot.send_message(
-        chat_id=chat_id,
-        text=answer,
-        parse_mode="HTML",
-        disable_web_page_preview=True,
-    )
+#     menu = [buttons[i : i + n_cols] for i in range(0, len(buttons), n_cols)]
+#     if header_buttons:
+#         menu.insert(0, [header_buttons])
+#     if footer_buttons:
+#         menu.append([footer_buttons])
+#     return menu
--- tests/unit/telegram/test_tracklist.py
+++ tests/unit/telegram/test_tracklist.py
@@ -1,12 +1,56 @@
 from promotrackerbot.clients.telegram.tracklist import tracklist
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 
-def test_tracklist():
+@patch("promotrackerbot.clients.telegram.tracklist.RedisClient")
+def test_tracklist(mock_redis):
     # Arrange
-    tracklist_ = ["Placeholder1", "Placeholder2"]
-    chat_id = 999
-    response = "These are the products you are tracking: " + ', '.join(tracklist_) + "."
+    chat_id = "999"
+    url = "https://store.steampowered.com/app/"
+    appid_1 = "1234"
+    game_1 = "test1"
+    appid_2 = "5678"
+    game_2 = "test2"
+    response_basic = "These are the products you are tracking"
+
+    redis = mock_redis.return_value
+    redis.get.return_value = {
+        appid_1: {"name": game_1, "price_formated": "0", "discount": 0},
+        appid_2: {"name": game_2, "price_formated": "0", "discount": 0},
+    }
+
+    update = Mock()
+    update.effective_chat.id = chat_id
+
+    context = Mock()
+
+    result = {}
+
+    def get_bot_answer(**kwargs):
+        result.update(kwargs)
+
+    context.bot.send_message = get_bot_answer
+
+    # Act
+    tracklist(update, context)
+
+    # Assert
+    assert result["chat_id"] == chat_id
+    assert response_basic in result["text"]
+    assert game_1 in result["text"]
+    assert url + appid_1 in result["text"]
+    assert game_2 in result["text"]
+    assert url + appid_2 in result["text"]
+
+
+@patch("promotrackerbot.clients.telegram.tracklist.RedisClient")
+def test_empty_tracklist(mock_redis):
+    # Arrange
+    chat_id = "999"
+    response = "No games are being tracked."
+
+    redis = mock_redis.return_value
+    redis.exists.return_value = False
 
     update = Mock()
     update.effective_chat.id = chat_id
